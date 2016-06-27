229ff1ff073afb15cf1e073c4e8ee87c1bfbc3e1
eg <eringoncer@gmail.com>
2016-06-26 20:33:42 -0700

    fix all issues, make favorites work

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,11 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  favoriteQueston: Ember.inject.service(),
+  favoriteQuestion: Ember.inject.service(),
   updateQuestionForm: false,
   actions: {
     addToFavorites(favorite) {
-      this.get('favoriteQueston').add(favorite);
+      this.get('favoriteQuestion').add(favorite);
     },
     update(question, params) {
       this.sendAction('update', question, params);
